a406f137db2a0aab3aa3d36250ce0b7735120bfe
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-14 01:54:41 +0200

    what a mess

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { AnimatePresence } from "framer-motion";
-import "../styles/glasscard.css";
+
+import "../styles/globals.css";
+import "../styles/footer.css";
 import "../styles/backgrounds.css";
+import "../styles/index.css"
+import "../styles/glasscard.css";
 import "../styles/opencard.css";
 
 export default function App({ Component, pageProps, router }: AppProps) {
