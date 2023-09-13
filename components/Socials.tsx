8440515ac0d5907b2908e98ca3e6107374a6fed9
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-14 01:56:33 +0200

    remove random divs

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -9,41 +9,26 @@ const Socials = () => {
     return (
         <div className="socials textfootercolor">
             {/* CHANGE FONT? */}
-
-            <div className="items-center">
-
-                <a className="socials__tags" target="_blank" href="mailto:matijap@stud.ntnu.no" rel="noreferrer">
-                    <Image src={mail} alt="Mail: " width={25} height={25}/>
-                    &nbsp;matijap@stud.ntnu.no
-                </a>
-
-            </div>
-
-            <div className="items-center">
-                <a className="socials__tags" target="_blank" href="https://www.github.com/makiv2" rel="noreferrer">
-                    <div className={"socials__icon"}>
-                        <Image src={github} alt="Github: " width={25} height={25}/>
-                    </div>
-                    <div className={"socials__text"}>
-                        &nbsp;github.com/makiv2
-                    </div>
-                </a>
-            </div>
-
-            <div className="items-center">
-                <a className="socials__tags" target="_blank" href="https://www.twitter.com" rel="noreferrer">
-                    <Image src={twitter} alt="Twitter: " width={25} height={25}/>
-                    &nbsp;twitter.com/????
-                </a>
-            </div>
-
-            <div className="items-center">
-                <a className="socials__tags" target="_blank" href="www.linkedin.com/in/matija-popovic00"
-                   rel="noreferrer">
-                    <Image src={linkedin} alt="Linkedin: " width={25} height={25}/>
-                    &nbsp;/in/matija-popovic
-                </a>
-            </div>
+            <a className="socials__tags" target="_blank" href="mailto:matijap@stud.ntnu.no" rel="noreferrer">
+                <Image src={mail} alt="Mail: " width={25} height={25}/>
+                &nbsp;matijap@stud.ntnu.no
+            </a>
+            <a className="socials__tags" target="_blank" href="https://www.github.com/makiv2" rel="noreferrer">
+                <div className={"socials__icon"}>
+                    <Image src={github} alt="Github: " width={25} height={25}/>
+                </div>
+                <div className={"socials__text"}>
+                    &nbsp;github.com/makiv2
+                </div>
+            </a>
+            <a className="socials__tags" target="_blank" href="https://www.twitter.com" rel="noreferrer">
+                <Image src={twitter} alt="Twitter: " width={25} height={25}/>
+                &nbsp;twitter.com/????
+            </a>
+            <a className="socials__tags" target="_blank" href="www.linkedin.com/in/matija-popovic00" rel="noreferrer">
+                <Image src={linkedin} alt="Linkedin: " width={25} height={25}/>
+                &nbsp;/in/matija-popovic
+            </a>
         </div>
     );
 }
