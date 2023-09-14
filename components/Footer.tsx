b12f0a265e60664c1d72e0d8161efc9956695665
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-14 02:09:52 +0200

    text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Socials from "./Socials";
 
 const Footer = () => {
   return (
-    <footer className="absolute w-full bottom-0 shadow-[0_-1px_5px_rgba(0,0,0,0.15)] h-14.3p flex flex-wrap justify-evenly colorchanger">
+    <footer className="footer shadow-[0_-1px_5px_rgba(0,0,0,0.15)] colorchanger">
         <Socials />
     </footer>
   );
