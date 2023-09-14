a0e66d398e8dbd0886d8529abb80695f27f66682
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-14 19:37:40 +0200

    guard for 375px and proto index finished

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { AnimatePresence } from "framer-motion";
+import {AnimatePresence} from "framer-motion";
 
 import "../styles/globals.css";
 import "../styles/footer.css";
@@ -11,14 +11,17 @@ import "../styles/index.css";
 import "../styles/glasscard.css";
 import "../styles/opencard.css";
 
-export default function App({ Component, pageProps, router }: AppProps) {
-  return (
-    <>
-      <Navbar />
-      <AnimatePresence initial={true} mode={"sync"}>
-        <Component key={router.pathname} {...pageProps} />
-      </AnimatePresence>
-      <Footer />
-    </>
-  );
+export default function App({Component, pageProps, router}: AppProps) {
+    return (
+        <>
+            <div className={"global__guard__min"}>You can not view this site</div>
+            <div className={"global__guard__max"}>
+                <Navbar/>
+                <AnimatePresence initial={true} mode={"sync"}>
+                    <Component key={router.pathname} {...pageProps} />
+                </AnimatePresence>
+                <Footer/>
+            </div>
+        </>
+    );
 }
