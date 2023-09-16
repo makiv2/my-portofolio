9ccbd8055cd7dcbfee1d8cd3c72fbfeab9af03ee
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-17 01:54:18 +0200

    static exp

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,23 +1,17 @@
-import { motion as m } from "framer-motion";
+import StaticExperience from "../components/Mobile/StaticExperience";
+import AnimatedExperience from "../components/Pc/AnimatedExperience";
+
 
 function Experience() {
   return (
-    <m.div
-      initial={{ y: "100%" }}
-      animate={{ y: "0%" }}
-      transition={{ duration: 0.4, ease: "easeOut" }}
-      exit={{ opacity: 0 }}
-      className="text-main-fourth absolute top-0 w-full h-85.7p experienceBackground backgroundStyle lg:px-32 px-16"
-    >
-      <main>
-        <div className="mt-80 absolute left-32">
-          <h1 className="text-6xl text-center lg:text-left lg:text-8xl px-15 font-rowdies">
-            Experience //TODO
-          </h1>
+      <>
+        <div className={"guard_mobile"}>
+          <StaticExperience></StaticExperience>
         </div>
-
-      </main>
-    </m.div>
+        <div className={"guard_pc"}>
+          <AnimatedExperience></AnimatedExperience>
+        </div>
+      </>
   );
 }
 
