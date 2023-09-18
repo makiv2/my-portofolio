5dca0228930a1519e6726a8e4200c9f621141577
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-18 23:46:51 +0200

    spaguetti code to fix on load colors and navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type {AppProps} from "next/app";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import {AnimatePresence} from "framer-motion";
 
 import "../styles/globals.css";
 import "../styles/index.css";
@@ -17,15 +16,7 @@ export default function App({Component, pageProps, router}: AppProps) {
     return (
         <>
             <Navbar/>
-            {/*<div className={"guard_pc"}>*/}
-            {/*    <AnimatePresence initial={true} mode={"sync"}>*/}
-            {/*        <Component key={router.pathname} {...pageProps} />*/}
-            {/*    </AnimatePresence>*/}
-            {/*</div>*/}
-            {/*<div className={"guard_mobile"}>*/}
                 <Component key={router.pathname} {...pageProps} />
-            {/*</div>*/}
-
             <Footer/>
         </>
     );
