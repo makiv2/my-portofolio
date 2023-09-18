55c8a33521136d990679fd686a659d820185f749
Matija Popovic <matijapopovic001212@gmail.com>
2023-09-18 23:46:51 +0200

    spaguetti code to fix on load colors and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,121 +1,150 @@
-import { useEffect, useRef } from "react";
+import {useEffect, useRef} from "react";
 import Link from "next/link";
-import { gsap } from "gsap";
+import {gsap} from "gsap";
 import Flip from "gsap/dist/Flip";
 
 // function for toggling dark mode
 gsap.registerPlugin(Flip);
 
 const Navbar = () => {
-  const links = useRef(new Array(3));
-  const activeNav = useRef<HTMLDivElement>(null);
+    const links = useRef(new Array(3));
+    const activeNav = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    //set active nav
-    gsap.to(links.current[0], { color: "rgb(34, 211, 238)" });
-    console.log("test");
-  }, []);
+    useEffect(() => {
+        // Put colors on initial load
+        let path = window.location.pathname.substring(1);
+        if (path === "") {
+            gsap.to(".navbar__link__main", {color: "rgb(34, 211, 238)"});
+        } else if (path === "projects") {
+            gsap.to(".navbar__link__projects", {color: "#c90000"});
+            gsap.to(".colorchanger", {backgroundColor: "#800000"});
+            gsap.to(".navbar", {"--border-color": "#800000"});
+            gsap.to(".navbar__slider", {backgroundColor: "#c90000"});
+            gsap.to(".textfootercolor", {color: "#e0e0e0"});
+        } else if (path === "about") {
+            gsap.to(".navbar__link__about", {color: "#8860d0"});
+            gsap.to(".colorchanger", {backgroundColor: "#c1c8e4"});
 
-  const handleClick = (e: Element) => {
-    links.current.forEach((link) => {
-      gsap.to(link, { color: "#e0e0e0" });
-    });
-
-    links.current.forEach((link) => {
-      if (link === e) {
-        if (e === links.current[0]) {
-          gsap.to(link, { color: "rgb(34, 211, 238)" });
-          gsap.to(".navbar__link", { color: "#e0e0e0" });
-        } else if (e === links.current[1]) {
-          gsap.to(link, { color: "#c90000" });
-          gsap.to(".navbar__link", { color: "#e0e0e0" });
-        } else if (e === links.current[2]) {
-          gsap.to(link, { color: "#8860d0" });
-          gsap.to(".navbar__link", { color: "#2f2f2f" });
+            gsap.to(".navbar", {"--border-color": "#8860d0"});
+            gsap.to(".navbar__slider", {backgroundColor: "#8860d0"});
+            gsap.to(".textfootercolor", {color: "#000000"});
         }
-      }
-    });
+    }, []);
 
-    //move line
-    const state = Flip.getState(activeNav.current);
+    useEffect(() => {
+        const state = Flip.getState(activeNav.current);
+        let path = window.location.pathname.substring(1);
+        if (path === "") {
 
-    if (activeNav.current !== null) {
-      console.log(e);
-      e.appendChild(activeNav.current);
-    }
-    Flip.from(state, { duration: 0.8, ease: "elastic.out(1, 0.5)" });
-    //ease: "power2.inOut"
+            links.current[0].appendChild(activeNav.current);
+        } else if (path === "projects") {
+            links.current[1].appendChild(activeNav.current);
+        } else if (path === "about") {
+            links.current[2].appendChild(activeNav.current);
+        }
 
-    //If element is the main page, change the color of the footer to blue, if the element is of the project page, change the color to red, if the element is of the experience page, change the color to green
+        Flip.from(state, {duration: 0.8, ease: "elastic.out(1, 0.5)"});
+    }, [activeNav]);
 
-    if (e === links.current[0]) {
-      gsap.to(".colorchanger", { backgroundColor: "#00ADB5" });
+    const handleClick = (e: Element) => {
+        links.current.forEach((link) => {
+            gsap.to(".navbar__link__main", {color: "#e0e0e0"});
+            gsap.to(".navbar__link__projects", {color: "#e0e0e0"});
+            gsap.to(".navbar__link__about", {color: "#e0e0e0"});
+        });
 
-      gsap.to(".navbar", { "--border-color": "#00ADB5" });
-      gsap.to(".navbar__slider", { backgroundColor: "rgb(34, 211, 238)" });
-      gsap.to(".textfootercolor", { color: "#000000" });
-    } else if (e === links.current[1]) {
-      gsap.to(".colorchanger", { backgroundColor: "#800000" });
+        links.current.forEach((link) => {
+            if (link === e) {
+                if (e === links.current[0]) {
+                    gsap.to(".navbar__link__main", {color: "rgb(34, 211, 238)"});
+                } else if (e === links.current[1]) {
+                    gsap.to(".navbar__link__projects", {color: "#c90000"});
+                } else if (e === links.current[2]) {
+                    gsap.to(".navbar__link__about", {color: "#8860d0"});
+                }
+            }
+        });
 
-      gsap.to(".navbar", { "--border-color": "#800000" });
-      gsap.to(".navbar__slider", { backgroundColor: "#c90000" });
-      gsap.to(".textfootercolor", { color: "#e0e0e0" });
-    } else if (e === links.current[2]) {
-      gsap.to(".colorchanger", { backgroundColor: "#c1c8e4" });
+        //move line
+        const state = Flip.getState(activeNav.current);
 
-      gsap.to(".navbar", { "--border-color": "#8860d0" });
-      gsap.to(".navbar__slider", { backgroundColor: "#8860d0" });
-      gsap.to(".textfootercolor", { color: "#000000" });
-    }
-  };
+        if (activeNav.current !== null) {
+            e.appendChild(activeNav.current);
+        }
+        Flip.from(state, {duration: 0.8, ease: "elastic.out(1, 0.5)"});
+        //ease: "power2.inOut"
 
-  return (
-      <nav className="navbar">
-        <ul className="navbar__ul text-main-fourth">
-          <div
-            ref={(el) => (links.current[0] = el)}
-            className="navbar__div"
-          >
-            <li>
-              <Link className={"navbar__link"} onClick={() => handleClick(links.current[0])} href="/">
-                Main
-              </Link>
-            </li>
-            <div
-              ref={activeNav}
-              className="navbar__slider"
-            ></div>
-          </div>
+        //If element is the main page, change the color of the footer to blue, if the element is of the project page, change the color to red, if the element is of the experience page, change the color to green
 
-          <div
-            ref={(el) => (links.current[1] = el)}
-            className="navbar__div"
-          >
-            <li>
-              <Link className={"navbar__link"}
-                onClick={() => handleClick(links.current[1])}
-                href="/projects"
-              >
-                Projects
-              </Link>
-            </li>
-          </div>
-          <div
-            ref={(el) => (links.current[2] = el)}
-            className="navbar__div"
-          >
-            <li>
-              <Link className={"navbar__link"}
-                onClick={() => handleClick(links.current[2])}
-                href="/about"
-              >
-                About
-              </Link>
-            </li>
-          </div>
-        </ul>
-      </nav>
-  );
+        if (e === links.current[0]) {
+            gsap.to(".colorchanger", {backgroundColor: "#00ADB5"});
+
+            gsap.to(".navbar", {"--border-color": "#00ADB5"});
+            gsap.to(".navbar__slider", {backgroundColor: "rgb(34, 211, 238)"});
+            gsap.to(".textfootercolor", {color: "#000000"});
+        } else if (e === links.current[1]) {
+            gsap.to(".colorchanger", {backgroundColor: "#800000"});
+
+            gsap.to(".navbar", {"--border-color": "#800000"});
+            gsap.to(".navbar__slider", {backgroundColor: "#c90000"});
+            gsap.to(".textfootercolor", {color: "#e0e0e0"});
+        } else if (e === links.current[2]) {
+            gsap.to(".colorchanger", {backgroundColor: "#c1c8e4"});
+
+            gsap.to(".navbar", {"--border-color": "#8860d0"});
+            gsap.to(".navbar__slider", {backgroundColor: "#8860d0"});
+            gsap.to(".textfootercolor", {color: "#000000"});
+        }
+    };
+
+    return (
+        <nav className="navbar">
+            <ul className="navbar__ul text-main-fourth">
+                <div
+                    ref={(el) => (links.current[0] = el)}
+                    className="navbar__div"
+                >
+                    <li>
+                        <Link className={"navbar__link navbar__link__main"}
+                              onClick={() => handleClick(links.current[0])} href="/">
+                            Main
+                        </Link>
+                    </li>
+                    <div
+                        ref={activeNav}
+                        className="navbar__slider"
+                    ></div>
+                </div>
+
+                <div
+                    ref={(el) => (links.current[1] = el)}
+                    className="navbar__div"
+                >
+                    <li>
+                        <Link className={"navbar__link navbar__link__projects"}
+                              onClick={() => handleClick(links.current[1])}
+                              href="/projects"
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                </div>
+                <div
+                    ref={(el) => (links.current[2] = el)}
+                    className="navbar__div"
+                >
+                    <li>
+                        <Link className={"navbar__link navbar__link__about"}
+                              onClick={() => handleClick(links.current[2])}
+                              href="/about"
+                        >
+                            About
+                        </Link>
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
